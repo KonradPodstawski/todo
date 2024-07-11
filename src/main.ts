6e859418de7f7d2ae0d7dd0151de463968ae2b0b
Konrad Podstawski <konradpodstawskii@gmail.com>
2024-07-11 14:41:01 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
   <div id="title-page" class="bg-gray-200 dark:bg-black flex items-center justify-center min-h-screen">
     <div class="text-center relative w-full max-w-md">
-      <h1 class="text-4xl font-bold text-blue-600 dark:text-blue-400">Hello, World!</h1>
-      <p class="text-lg text-gray-700 dark:text-gray-300 mt-4">This is a <a class="underline decoration-indigo-500">Tailwind</a> CSS example.</p>
+      <h1 class="text-4xl font-bold text-blue-600 dark:text-blue-400">TODO TODO</h1>
+      <p class="text-lg text-gray-700 dark:text-gray-300 mt-4">Ugliest <a class="underline decoration-indigo-500">Menage</a> APP.</p>
       ${getAuthHtml()}
     </div>
   </div>
